72b348799a394eb2826503a466b08afb7d16be5c
ktlwap <ktlwap@gmail.com>
2024-03-09 19:02:52 +0100

    UI overhaul

--- Hades.Studio/Models/ComponentDetails.cs
+++ Hades.Studio/Models/ComponentDetails.cs
@@ -3,9 +3,11 @@ namespace Hades.Studio.Models;
 public class ComponentDetails
 {
     public string Name { get; set; }
+    public bool Enabled { get; set; }
 
     public ComponentDetails(string name)
     {
         Name = name;
+        Enabled = true;
     }
 }
