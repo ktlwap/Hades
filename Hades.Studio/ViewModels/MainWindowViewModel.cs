624f074772e9c5785595ea4ba4e6edda1b54629d
ktlwap <149824361+ktlwap@users.noreply.github.com>
2024-03-09 10:12:24 +0100

    Implement hierarchy view (#2)

--- Hades.Studio/ViewModels/MainWindowViewModel.cs
+++ Hades.Studio/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,8 @@
-﻿namespace Hades.Studio.ViewModels;
+﻿using Hades.Studio.Models;
+
+namespace Hades.Studio.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public HierarchyViewModel HierarchyViewModel { get; } = new();
 }
---
+++ Hades.Studio/Views/HierarchyView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Hades.Studio.Views;
+
+public partial class HierarchyView : UserControl
+{
+    public HierarchyView()
+    {
+        InitializeComponent();
+    }
+}
